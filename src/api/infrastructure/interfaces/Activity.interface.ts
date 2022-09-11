d3da307400d5c8028f31227236b7406bcb753eaf
Gabriel Murta Lima <gabrielmurtalima99@gmail.com>
2022-09-11 18:39:27 -0300

    fixing issue

--- src/api/infrastructure/interfaces/Activity.interface.ts
+++ src/api/infrastructure/interfaces/Activity.interface.ts
@@ -2,4 +2,3 @@ export interface Activities {
     id:string;
     name:string;
   }
-  
--- src/api/infrastructure/providers/dynamoDB/dynamoDbAdapter.module.ts
+++ src/api/infrastructure/providers/dynamoDB/dynamoDbAdapter.module.ts
@@ -8,3 +8,4 @@ import { DynamoDBAdapter } from './dynamoDbAdapter';
   exports: [DynamoDBAdapter],
 })
 export class DynamoDBAdapterModule {}
+
