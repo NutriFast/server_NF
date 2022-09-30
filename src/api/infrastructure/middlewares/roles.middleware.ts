e95f3b92e3a0122dad3fb563777280552dc9b046
Gabriel Murta Lima <gabriellima@ioasys.com.br>
2022-09-30 14:04:08 -0300

    fixing issue when not founding a user on role middleware

--- src/api/infrastructure/middlewares/roles.middleware.ts
+++ src/api/infrastructure/middlewares/roles.middleware.ts
@@ -1,10 +1,11 @@
-import { ForbiddenException } from "@nestjs/common";
+import { ForbiddenException, NotFoundException } from "@nestjs/common";
 import { UsersService } from "src/api/endpoints/users/users.service";
 import { Role } from "../constants/roles";
 
 export class RolesMiddleware {
   constructor(public userService: UsersService) {}
   public async isMaster(user: any) {
+    if(!user) throw new NotFoundException('user-not-found')
     if (user.role == Role.master) return true;
     throw new ForbiddenException("not-a-master-user");
   }
