9411504f78c4aba0b37578e600ae147be8efae92
Gabriel Murta Lima <gabrielmurtalima99@gmail.com>
2022-12-11 14:14:22 -0300

    final touchs to presentation

--- src/api/endpoints/activitySchedules/activitySchedules.service.ts
+++ src/api/endpoints/activitySchedules/activitySchedules.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Injectable,
   NotFoundException,
 } from "@nestjs/common";
@@ -30,6 +31,9 @@ export class ActivitySchedulesService {
     } catch (err) {
       throw new NotFoundException(err);
     }
+    if(!result){
+      result = []
+    }
     result = await Promise.all(
       result.map((item) => {
         return this.getDBAndPT(item);
@@ -42,13 +46,16 @@ export class ActivitySchedulesService {
       sumPT += item.parcialTime;
     });
     const TPR = (1440 - sumDB) * 1.5;
-    const FAF = sumPT + TPR / 1440;
+    let FAF = (sumPT + TPR) / 1440;
+    FAF = Math.round(FAF * 100) / 100
     let AF;
     let status;
     const schedule = await this.scheduleRepository.getById(scheduleId);
     const client = await this.clientRepository.getById(schedule.clientId);
+    const age = await this.getAge(client.birthDate)
+    let GE;
     if (client.gender == "Masculino") {
-      if (FAF >= 1 && FAF < 1.4) {
+      if (FAF < 1.4) {
         AF = 1.0;
         status = "Sedentário";
       }
@@ -60,10 +67,23 @@ export class ActivitySchedulesService {
         AF = 1.25;
         status = "Ativo";
       }
-      if (FAF >= 1.9 && FAF < 2.5) {
+      if (FAF >= 1.9) {
         AF = 1.48;
         status = "Muito Ativo";
       }
+      if(AF) {
+        if(age >= 3 && age <= 8) {
+          GE = 88.5 - (61.9 * age) + (AF * ((26.7 * client.weight) + (903 * client.height) + 20))
+        }
+        if(age >= 9 && age <= 18) {
+          GE = 88.5 - (61.9 * age) + (AF * ((26.7 * client.weight) + (903 * client.height) + 25))
+        }
+        if(age >= 19) {
+          GE = 662 - (9.53 * age) + (AF * ((15.91 * client.weight) + (539.6 * client.height)))
+        }
+  
+      }
+        
     } else {
       if (FAF >= 1 && FAF < 1.4) {
         AF = 1.0;
@@ -81,7 +101,17 @@ export class ActivitySchedulesService {
         AF = 1.45;
         status = "Muito Ativo";
       }
+      if(age >= 3 && age <= 8) {
+        GE = 135.3 - (30.8 * age) + (AF * ((10 * client.weight) + (934 * client.height) + 20))
+      }
+      if(age >= 9 && age <= 18) {
+        GE = 135.3 - (30.8 * age) + (AF * ((10 * client.weight) + (934 * client.height) + 25))
+      }
+      if(age >= 19) {
+        GE = 354 - (6.91 * age) + (AF * ((9.36 * client.weight) + (726 * client.height)))
+      }
     }
+    GE = Math.round(GE * 100) / 100
     const payload = {
       result,
       sumDB,
@@ -90,10 +120,11 @@ export class ActivitySchedulesService {
       FAF,
       AF,
       status,
+      GE,
     };
+    console.log(payload)
     return payload;
   }
-
   public async create(dto: CreateActivityScheduleDTO, clientId: string) {
     const document = new ActivitiesScheduleDocument();
     if (dto.scheduleId) {
@@ -120,8 +151,12 @@ export class ActivitySchedulesService {
     return this.repository.create(document);
   }
   private async getDBAndPT(item: ActivitiesScheduleDocument) {
-    item.dailyBase = (item.freequency * item.duration) / 7;
+    item.dailyBase = (item.freequency * 60 * item.duration) / 7;
     const activity = await this.activityRepository.getById(item.activityId);
+    if(!activity) {
+      console.log(`failed to find activityId: ${item.activityId}`)
+      throw new BadRequestException(`failed to find activityId: ${item.activityId}`)
+    }
     item.parcialTime = activity.value * item.dailyBase;
     return item;
   }
@@ -129,4 +164,15 @@ export class ActivitySchedulesService {
   public async delete(id:string) {
     return this.repository.deleteById(id);
   }
+
+  private getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
 }
